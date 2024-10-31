f77668e5e818d411ed6c231f2e1959abbe5f985d
matan <jerbimatan@gmail.com>
2024-10-31 16:15:27 +0200

    more some queries

--- schema.py
+++ schema.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
@@ -43,22 +44,49 @@ class Query(graphene.ObjectType):
     #find mission by country
     missions_by_country = graphene.List(MissionsType, country_id=graphene.Int(required=True))
     #find mission by target industry
-    missions_by_target_industry = graphene.List(MissionsType, target_industry_id=graphene.Int(required=True))
+    missions_by_target_industry = graphene.List(MissionsType, target_industry=graphene.String(required=True))
+
 
 
     def resolve_mission_by_id(self, info, id):
         return db_session.query(MissionsModel).get(id)
 
     def resolve_missions_by_date_range(self, info, start_date, end_date):
-        return db_session.query((MissionsModel).filter(MissionsModel.mission_date >= start_date, MissionsModel.mission_date <= end_date).all()
+        session = db_session()
+        start = datetime.strptime(start_date, '%Y-%m-%d').date()
+        end = datetime.strptime(end_date, '%Y-%m-%d').date()
+
+        return (
+            session.query(MissionsModel)
+            .filter(MissionsModel.mission_date >= start)
+            .filter(MissionsModel.mission_date <= end)
+            .all()
+        )
+
+
 
     def resolve_missions_by_country(self, info, country_id):
-        return db_session.query(MissionsModel).filter(MissionsModel.country_id == country_id).all()
+        session = db_session()
+        return (
+            session.query(MissionsModel)
+            .join(MissionsModel.targets)
+            .join(TargetModel.city)
+            .join(CitiesModel.country)
+            .filter(CountriesModel.country_id == country_id)
+            .all()
+        )
 
-    def resolve_missions_by_target_industry(self, info, target_industry_id):
-        return db_session.query(MissionsModel).filter(MissionsModel.target_industry_id == target_industry_id).all()
+    def resolve_missions_by_target_industry(self, info, target_industry):
+        session = db_session()
+        return (
+            session.query(MissionsModel)
+            .join(MissionsModel.targets)
+            .filter(TargetModel.target_industry == target_industry)
+            .all()
+        )
 
 
 
+#
 
 schema = graphene.Schema(query=Query)
